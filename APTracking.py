18909c1a83a803bff060d2c5fd86b85deedd290e
Dominik Wagner <Roemml@gmx.de>
2025-07-13 12:08:48 +0200

    radiobutton

--- APTracking.py
+++ APTracking.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import os
+import sys
 from ftplib import FTP_TLS
+import mysql.connector
 import urllib.request
 from bs4 import BeautifulSoup
 import Roemdules
@@ -12,6 +14,10 @@ class Config():
         self.url = ""
         self.user = ""
         self.passwort = ""
+        self.dbname = ""
+def beenden():
+    # fenster.destroy()
+    sys.exit("Programm vorzeitig beendet") 
 def get_config_from_window(fenster:tk.Tk, config: Config):
     for child in fenster.children.values():
         if("name" in child.__dict__): 
@@ -25,6 +31,8 @@ def get_config_from_window(fenster:tk.Tk, config: Config):
                 config.user = child.get()
             elif child.name == "Passwort":
                 config.passwort = child.get()
+            elif child.name == "DBName":
+                config.dbname = child.get()
     fenster.destroy()
 def get_config_from_file(file_name: str, config: Config):
     with open(file_name, "r") as config_datei:
@@ -42,7 +50,9 @@ def get_config_from_file(file_name: str, config: Config):
                 elif argument.upper() == "USER":
                     config.user = wert.strip()
                 elif argument.upper() == "PW":
-                    config.passwort = wert.strip() 
+                    config.passwort = wert.strip()
+                elif argument.upper() == "DATABASE":
+                    config.dbname = wert.strip()
                 else:
                     raise NameError(f"Ungültiger Parameter in der Config: {argument}", name=argument)
             else:
@@ -89,35 +99,69 @@ def FTP_write(ftpurl,ftpuser,ftppw,file_name,data:dict):
             except Exception as e:
                 success = False
     if success == True: os.remove(file_name)
+def DB_read(mycursor,tracker,name):
+    alt = {}
+    mycursor.execute('SELECT * FROM APTracking WHERE ID = %s and Game = %s', (tracker, name))
+    for line in mycursor.fetchall(): 
+        dummy1, dummy2, item, anzahl = line
+        alt[item] = anzahl
+    return alt
+def DB_write_update(mycursor,tracker:str,name:str,alt:dict,neu:dict):
+    for item, anzahl in neu.items():
+        if (item in alt):
+            if int(anzahl) > int(alt[item]):
+                mycursor.execute('UPDATE APTracking SET Anzahl = %s WHERE ID = %s AND Game = %s AND Item = %s', (anzahl, tracker, name, item))
+        else:
+            mycursor.execute('INSERT INTO APTracking (ID, Game, Item, Anzahl) VALUES(%s, %s, %s, %s)', (tracker, name, item, anzahl))
+def DB_write_clean(mycursor,tracker:str,name:str,neu:dict):
+    values = []
+    for item, anzahl in neu.items():
+        values.append((tracker, name, item, int(anzahl)))
+    mycursor.execute('DELETE FROM APTracking WHERE ID = %s AND Game = %s', (tracker, name))
+    mycursor.executemany('INSERT INTO APTracking (ID, Game, Item, Anzahl) VALUES (%s, %s, %s, %s)', values)
+
 ###############################################################################################
 
 try:
     config = Config()
-    fenster = Roemdules.gui.erstelle_Fenster(
+    # modus = tk.StringVar()
+    # modus = "DB"
+    fenster, varlist = Roemdules.gui.erstelle_Fenster(
         [{"type":"label", "text":"Spieler (Teil)Name:", "align":Roemdules.gui.ALIGN_CENTER}
-        ,{"type":"entry", "width":20, "align":Roemdules.gui.ALIGN_LEFT, "name":"Name"}
-        ,{"type":"label", "text":"AP Tracker ID", "align":Roemdules.gui.ALIGN_CENTER}
-        ,{"type":"entry", "width":20, "align":Roemdules.gui.ALIGN_LEFT, "name":"Tracker"}
+        ,{"type":"entry", "width":20, "name":"Name", "align":Roemdules.gui.ALIGN_LEFT}
+        ,{"type":"label", "text":"AP Tracker ID", "name":"Tracker", "align":Roemdules.gui.ALIGN_CENTER}
+        ,{"type":"entry", "width":20, "align":Roemdules.gui.ALIGN_LEFT}
         ,{"type":"label", "text":"FTP/DB Url:", "align":Roemdules.gui.ALIGN_CENTER}
-        ,{"type":"entry", "width":20, "align":Roemdules.gui.ALIGN_LEFT, "name":"Url"}
+        ,{"type":"entry", "width":20, "name":"Url", "align":Roemdules.gui.ALIGN_LEFT}
         ,{"type":"label", "text":"FTP/DB UserName:", "align":Roemdules.gui.ALIGN_CENTER}
-        ,{"type":"entry", "width":20, "align":Roemdules.gui.ALIGN_LEFT, "name":"User"}
+        ,{"type":"entry", "width":20, "name":"User", "align":Roemdules.gui.ALIGN_LEFT}
         ,{"type":"label", "text":"FTP/DB Passwort:", "align":Roemdules.gui.ALIGN_CENTER}
-        ,{"type":"entry", "width":20, "show":"*", "align":Roemdules.gui.ALIGN_LEFT, "name":"Passwort"}
+        ,{"type":"entry", "width":20, "show":"*", "name":"Passwort", "align":Roemdules.gui.ALIGN_LEFT}
+        ,{"type":"label", "text":"DB DBName:", "align":Roemdules.gui.ALIGN_CENTER}
+        ,{"type":"entry", "width":20, "name":"DBName", "align":Roemdules.gui.ALIGN_LEFT}
+        ,{"type":"radiobutton", "variable":"modus", "value":"FTP", "text": "Speichern per FTP", "align":Roemdules.gui.ALIGN_LEFT}
+        ,{"type":"radiobutton", "variable":"modus", "value":"DB", "text": "Speichern per DB", "align":Roemdules.gui.ALIGN_LEFT}
+        ,{"type":"radiobutton", "variable":"modus", "value":"DB-CLEAN", "text": "Speichern per DB mit vorher leeren", "align":Roemdules.gui.ALIGN_LEFT}
+        ,{"type":"radiobutton", "variable":"modus", "value":"DB_DEL", "text": "Löschen aller DB Daten mit AP ID", "align":Roemdules.gui.ALIGN_LEFT}
+        ,{"type":"radiobutton", "variable":"modus", "value":"DB_DELALL", "text": "Löschen aller DB Daten", "align":Roemdules.gui.ALIGN_LEFT}
         ,{"type":"button", "text":"Start", "command":"get_config_from_window(fenster, config)", "width":5, "height":1, "align":Roemdules.gui.ALIGN_CENTER}]
-        ,fenster_name = "Start"#, protocols = (("WM_DELETE_WINDOW", "start_fenster_schliessen(fenster,game_state)"),)
-        ,context = {'config': config, 'get_config_from_window': get_config_from_window}
+        ,fenster_name = "Start", protocols = (("WM_DELETE_WINDOW", "beenden()"),)
+        ,context = {'beenden': beenden, 'config': config, 'get_config_from_window': get_config_from_window, 'modus': 'DB'}
         )
+    #varlist["modus"].set("DB")
     fenster.mainloop()
+    modus_value:tk.StringVar = varlist["modus"].get()
+    print(modus_value)
     if (config.name == config.tracker == config.url == config.user == config.passwort == ""):
         get_config_from_file("APTracking.config", config)
+    mydb = mysql.connector.connect(host=config.url,user=config.user,password=config.passwort,database=config.dbname)
+    mycursor = mydb.cursor()
     link = f"https://archipelago.gg/tracker/{config.tracker}"
     seite = urllib.request.urlopen(link)
     soup = BeautifulSoup(seite, "html.parser")
     aptracker = soup.find("table", id="checks-table")
     tds = aptracker.find_all("td")
     for i in range(len(tds)-7):
-        # print(f"{i % 7}: {aptracker_tds[i]}")
         if i % 7 == 0:
             link = f'https://archipelago.gg{tds[i].find("a")["href"]}'
         elif i % 7 == 1:
@@ -126,11 +170,12 @@ try:
         elif i % 7 == 2 and name.count(config.name) > 0:
             game = str(tds[i].contents[0])
             neu = einzeltracker_drurchgehen(link)
-            alt = FTP_read(config.url,config.user,config.passwort,file_name)
+            # alt = FTP_read(config.url,config.user,config.passwort,file_name)
+            alt = DB_read(mycursor,config.tracker,name)
             diff = [f"game: {game}", " "]
             for item, anzahl in neu.items():
                 if (item in alt):
-                    if anzahl > alt[item]:
+                    if int(anzahl) > int(alt[item]):
                         diff.append(f"{item}: {int(anzahl) - int(alt[item])}")
                 else:
                     diff.append(f"{item}: {anzahl}")
@@ -139,7 +184,9 @@ try:
                 widgets.append({"type":"label", "text":difference, "align":Roemdules.gui.ALIGN_LEFT})
             fenster = Roemdules.gui.erstelle_Fenster(widgets,fenster_name = game)
             fenster.mainloop()
-            FTP_write(config.url,config.user,config.passwort,file_name,neu)
+            # FTP_write(config.url,config.user,config.passwort,file_name,neu)
+            DB_write_update(mycursor,config.tracker,name,alt,neu)
+            mydb.commit()
         else:   
             pass
 except Exception as e:
