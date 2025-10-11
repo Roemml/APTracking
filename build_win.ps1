f95df50bb4b6249ad520e9877c281b36c4a0c478
Dominik Wagner <Roemm@gmx.de>
2025-10-11 08:35:56 +0200

    build_win fix

--- build_win.ps1
+++ build_win.ps1
@@ -1,3 +1,3 @@
-#Remove-Item .\dist\* -Recurse
+Remove-Item .\dist\AP* -Recurse
 pyinstaller --onefile --windowed --log-level WARN apTracking.py
 Compress-Archive .\dist\* .\dist\APTracking_Win64.zip -Force
